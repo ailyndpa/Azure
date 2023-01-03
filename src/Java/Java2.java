83c9e6af5ddee4d3a248547c7841006eba2d42e0
Ailyn del Pino Acosta <ailyndpa@gmail.com>
2023-01-03 12:38:24 -0300

    "Ejemplo para curso azure"

--- src/Java/Java2.java
+++
@@ -1,9 +0,0 @@
-package Java;
-
-public class Java2 {
-	
-	 public String Nombre() {
-		 System.out.println("Ailyn!!");
-		 return "Ailyn del Pino!!!";
-	 }
-}
--- src/Selenium/TestNG/TestCase1.java
+++ src/Selenium/TestNG/TestCase1.java
@@ -2,34 +2,23 @@ package Selenium.TestNG;
 
 import org.testng.annotations.Test;
 
-import java.awt.RenderingHints.Key;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
 public class TestCase1 {
 	protected WebDriver driver;
+	WebElement round_Trip;
 
 	@BeforeTest
 	@Parameters({ "URL", "BrowserType" })
@@ -39,9 +28,17 @@ public class TestCase1 {
 			System.setProperty("webdriver.chrome.driver", "./src/resources/chromedriver.exe");
 			driver = new ChromeDriver();
 			break;
+		case "Edge":
+			System.setProperty("webdriver.edge.driver", "./src/resources/msedgedriver.exe");
+			driver= new EdgeDriver();
+			break;
+
 		case "Firefox":
 			System.setProperty("webdriver.gecko.driver", "./src/resources/geckodriver.exe");
-			driver = new FirefoxDriver();
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--headless");
+			driver = new FirefoxDriver(options);
+			
 			break;
 		default:
 			throw new Exception("Browser" + BrowserType + " not supported");
@@ -50,51 +47,48 @@ public class TestCase1 {
 		driver.manage().window().maximize();
 	}
 
-	@Test(dataProvider = "getData")
-	public void contactUs(String name, String subject, String sms, String email ) {
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scroll(0,1000)");
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
-		
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='button']/span[text()='Contact us']")));
-		driver.findElement(By.xpath("//button[@type='button']/span[contains(text(),'Contact us')]")).click();
-		driver.findElement(By.id("cf-1")).sendKeys(name);
-		driver.findElement(By.cssSelector("input[name='your-subject']")).sendKeys(subject);
-		driver.findElement(By.cssSelector("textarea[name='your-message']")).sendKeys(sms);
-
-		driver.findElement(By.xpath("//input[@type='email']")).sendKeys(sms);
-		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		
-		wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//span[@class='wpcf7-not-valid-tip']"),"The e-mail address entered is invalid."));
-		String errorMessage = driver.findElement(By.xpath("//span[@class='wpcf7-not-valid-tip']")).getText();
-		Assert.assertTrue(errorMessage.equals("The e-mail address entered is invalid."),"The error message displayed is not as expected.");
-		driver.findElement(By.id("cf-1")).clear();
-		driver.findElement(By.cssSelector("input[name='your-subject']")).clear();
-		driver.findElement(By.cssSelector("textarea[name='your-message']")).clear();
-		driver.findElement(By.xpath("//input[@type='email']")).clear();
-		Actions keyboard= new Actions(driver);
-		keyboard.keyDown(Keys.ESCAPE).build().perform();
+	@Test
+	public void selectCountry(){
+		driver.findElement(By.id("autosuggest")).sendKeys("Ha");
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<WebElement> list = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+																			
+		for (WebElement elem : list) {
+			if (elem.getText().equalsIgnoreCase("Haiti")) {
+				elem.click();
+				break;
+			}
+		}
 	}
-
-	@DataProvider
-	public Object[][] getData() {
-		Object[][] data = new Object[3][4];
-		data[0][0] = "Ailyn";
-		data[0][1] = "First test";
-		data[0][2] = "This is the message of the first test";
-		data[0][3] = "usuario@";
-		
-		data[1][0] = "Geider";
-		data[1][1] = "Probando";
-		data[1][2] = "sss";
-		data[1][3] = "usuario.com";
-		
-		data[2][0] = "Mama";
-		data[2][1] = "ggg";
-		data[2][2] = "dsdsddssdsdd";
-		data[2][3] = "usuario@*";
-		
-		return data;
+	@Test
+	public void verifRadioButton(){
+		WebElement oneWay = driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_0"));
+				round_Trip = driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1"));
+		round_Trip.click();
+		if (oneWay.isSelected()) {
+			WebElement Date = driver.findElement(By.xpath("//div/span[@class='date-close-disabled']"));
+			Assert.assertTrue(Date.isEnabled(),"Los campos asociados a la fecha deben estar deshabilitados cuando se selecciona la opción One Way");
+		} else if (round_Trip.isSelected()) {
+				WebElement Date = driver.findElement(By.xpath("//div/span[@class='date-close']"));
+				Assert.assertTrue(Date.isEnabled(),"Los campos asociados a la fecha deben estar habilitados cuando se selecciona la opción Round Trip");
+		}
+	}
+	@Test
+	public void fromTo(){
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR']//a[@value='BLR']")).click();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR']//a[@text='Hubli (HBX)']")).click();
 	}
 
 	@AfterClass
